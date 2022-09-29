a87048978f78ec7166c0232ee697935fa3e16847
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2022-09-29 16:32:49 +0500

    conditional movie rendering

---
+++ src/components/movieComponent.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (e) => {
+    const movies = this.state.movies.filter((m) => m._id !== e._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    let { length: count } = this.state.movies;
+
+    if (count === 0) return <h1>No movies in the database</h1>;
+
+    return (
+      <>
+        <h1>Showing {this.state.movies.length} Movies:</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
