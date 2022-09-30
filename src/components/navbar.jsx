2b288f0ddae4868fc5954907e20a70c546fed49a
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2022-09-30 10:47:06 +0500

    converted navbar into stateless component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import { Badge } from "react-bootstrap";
-class NavBar extends Component {
-  render() {
-    return (
-      <div className="container">
-        <nav className="navbar navbar-expand-lg bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar{" "}
-              <Badge className="bg-success">{this.props.totalCounters}</Badge>
-            </a>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+
+const NavBar = (props) => {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar <Badge className="bg-success">{props.totalCounters}</Badge>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
 export default NavBar;
