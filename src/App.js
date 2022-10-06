a75249ba248eeb7c6412c9462973e06f44ec3729
Abdul Aziz Khan <abdul.aziz.khan.1122@gmail.com>
2022-10-06 18:12:20 +0500

    optional Params addded

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Home from "./components/home";
+import ProductDetails from "./components/productDetails";
 import Dashboard from "./components/admin/dashboard";
 
 
 
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div >
         <NavBar />
         <div className="content">
-          <Routes>
 
+          <Routes>
+            <Route path={"/products/:id"} element={<ProductDetails />} />
             <Route path={"/products"} element={<Products />} />
-            <Route path={"/posts"} element={<Posts />} />
+            <Route path="/posts/"  >
+              <Route path=":year/" element={<Posts />} />
+              <Route path=":month/" element={<Posts />} />
+              <Route path="" element={<Posts />} />
+
+            </Route>
             <Route path={"/admin"} element={<Dashboard />} />
             <Route path={"/"} element={<Home />} />
           </Routes>
